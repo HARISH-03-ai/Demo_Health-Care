75f0a142eb6c3b11f19129b18b894e396167609d
Harish Vishwakarma <hv399106@gmail.com>
2025-12-20 06:39:22 +0530

    Remove hardcoded SMTP credentials

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'hv399106@gmail.com'
-app.config['MAIL_PASSWORD'] = 'eshb dhbs uwcl uaez'  # normal password nahi chalega
+app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
+app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")  # normal password nahi chalega
 
 mail = Mail(app)
 
